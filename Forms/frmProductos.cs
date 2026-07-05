5b9e23a1579e56903947d67e2f3cae77888e6c3d
jhoanir flores <jhoanirismael@gmail.com>
2026-07-05 13:14:18 -0600

    se le agrego paramettros al boton de nuevo y editar asi como seleccionar

--- Forms/frmProductos.cs
+++ Forms/frmProductos.cs
@@ -15,33 +15,150 @@ namespace Punto.Forms
         }
         private void frmProductos_Load(object sender, EventArgs e)
         {
-            CargarDatos();
+            Cargardatos();
+
         }
-        private void CargarDatos()
+        private void Cargardatos()
         {
             Conexion db = new Conexion();
             MySqlConnection con = db.ObtenerConexionAbierta();
-            if (con.State == ConnectionState.Open)
+            if (con != null && con.State == ConnectionState.Open)
             {
                 try
                 {
-                    string query = "SELECT id_producto, codigo, descripcion, precio, stock FROM productos";
+                    string query = "SELEC productos_id, codigo, descripcion, precio, stock FROM productos";
                     MySqlDataAdapter adapter = new MySqlDataAdapter(query, con);
                     DataTable tablaproductos = new DataTable();
                     adapter.Fill(tablaproductos);
                     dataGridView1.DataSource = tablaproductos;
-
                 }
                 catch (Exception ex)
                 {
-                    MessageBox.Show("Error al cargar los datos: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    MessageBox.Show("Error al cargar el catálogo: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    con.Close();
+                }
+
+            }
+        }
+        //nuevo
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            decimal preciovalido;
+            int stockvalido;
+
+            if(!decimal.TryParse(txtPrecio.Text, out preciovalido))
+            {
+                MessageBox.Show("Ingresa un precio valido", "Formato invalido", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (!int.TryParse(txtStock.Text, out stockvalido))
+            {
+                MessageBox.Show("Ingresa un stock valido", "Error de formato", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+
+            }
+            Conexion db = new Conexion();
+            MySqlConnection con = db.ObtenerConexionAbierta();
+
+            if (con != null && con.State == ConnectionState.Open)
+            {
+                try
+                {
+                    string query = "INSERT INTO productos (codigo, descripcion, precio, stock) VALUES (@codigo, @desc, @precio, @stock)";
+                    MySqlCommand cmd = new MySqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@codigo", txtNombre.Text);
+                    cmd.Parameters.AddWithValue("@desc", cmbCategorias.Text);
+                    cmd.Parameters.AddWithValue("@precio", preciovalido);
+                    cmd.Parameters.AddWithValue("@stock", stockvalido);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Producto registrado correctamente", "Exitoso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    
+                    //Limpiarformulario();
+                    Cargardatos();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al guardar: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    con.Close();
+                }
+
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow fila = dataGridView1.Rows[e.RowIndex];
+                string idString = fila.Cells["producto_id"].Value.ToString();
+                string nombre = fila.Cells["codigo"].Value.ToString();
+                string categoria = fila.Cells["descripcion"].Value.ToString();
+                string precio = fila.Cells["precio"].Value.ToString();
+                string stock = fila.Cells["stock"].Value.ToString();
+                idProductoseleccionado = Convert.ToInt32(idString);
+                txtNombre.Text = nombre;
+                txtPrecio.Text = precio;
+                txtStock.Text = stock;
+                cmbCategorias.Text = categoria;
+            }
+        }
+        //editar
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            if (idProductoseleccionado == 0)
+            {
+                MessageBox.Show("Por favor, seleccione un producto de la tabla primero.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            decimal preciovalido;
+            int stockvalido;
+            if (!decimal.TryParse(txtPrecio.Text, out preciovalido) || !int.TryParse(txtStock.Text, out stockvalido))
+            {
+                MessageBox.Show("Verifique que los campos numéricos sean válidos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+
+            }
+            Conexion db = new Conexion();
+            MySqlConnection con = db.ObtenerConexionAbierta();
+
+            if (con != null && con.State == ConnectionState.Open)
+            {
+                try
+                {
+                    string query = "UPDATE productos SET codigo = @codigo, descripcion = @desc, precio = @precio, stock = @stock WHERE producto_id = @id";
+                    MySqlCommand cmd = new MySqlCommand(query, con);
+
+                    cmd.Parameters.AddWithValue("@codigo", txtNombre.Text);
+                    cmd.Parameters.AddWithValue("@desc", cmbCategorias.Text);
+                    cmd.Parameters.AddWithValue("@precio", preciovalido);
+                    cmd.Parameters.AddWithValue("@stock", stockvalido);
+                    cmd.Parameters.AddWithValue("@id", idProductoseleccionado);
+
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Producto actualizado correctamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    //Limpiarformulario();
+                    Cargardatos();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al actualizar: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
                 finally
                 {
                     con.Close();
                 }
             }
+
         }
+        //eliminar
+
 
     }
 }
